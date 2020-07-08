3df5db25765ef0a60391ef5d74ef823dff7d74bf
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-07-08 17:03:42 +0200

    add: tutorial Apple about SwiftUI

---
+++ TutorialSwiftUI/TutorialSwiftUI/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  TutorialSwiftUI
+//
+//  Created by Valentin CHEVREAU on 08/07/2020.
+//  Copyright © 2020 MDS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TutorialSwiftUI/TutorialSwiftUI/LandmarkRow.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
